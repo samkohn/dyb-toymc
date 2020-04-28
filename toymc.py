99caa65f998b2f02b5ed4b5217811d64df1c8e96
Sam Kohn <kohn@berkeley.edu>
2020-04-27 22:56:35 -0700

    Update script portion of toymc.py to add nGd, nH, and muon events

--- toymc.py
+++ toymc.py
@@ -227,15 +227,22 @@ def main(outfile, runtime, seed):
     """Run the ToyMC with the given configuration."""
     toymc = ToyMC(outfile, runtime, seed=seed)
     single = Single("Single event", 20, 1, 1)
-    correlated = Correlated("IBD", 1, 1, 0.17, 28000)
+    ibd_nGd = Correlated("IBD nGd", 1, 1, 0.07, 28000)
+    ibd_nH = Correlated("IBD nH", 1, 1, 0.06, 150000)
+    ibd_nH.delayed_energy_spectrum = lambda rng: rng.uniform(1.9, 2.3)
+    ibd_nH.prompt_delayed_distance_mm = 100
+    muon = Muon("Muon", 1, 200)
     toymc.add_event_type(single)
-    toymc.add_event_type(correlated)
+    toymc.add_event_type(ibd_nGd)
+    toymc.add_event_type(ibd_nH)
+    toymc.add_event_type(muon)
     toymc.run()
 
 
 if __name__ == "__main__":
     from single import Single
     from correlated import Correlated
+    from muon import Muon
 
     parser = argparse.ArgumentParser(description="Daya Bay Toy MC by Sam Kohn")
     parser.add_argument("outfile")
